47057a97ae5d72bde81d89a5a772c128721e5afc
GiovanniTrevisan <gitrevizzo@hotmail.com.br>
2020-06-19 19:33:31 -0300

    Atualizado

--- app/src/main/java/com/viccontracovid/gamejam/MainActivity.kt
+++ app/src/main/java/com/viccontracovid/gamejam/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.viccontracovid.gamejam
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
@@ -22,5 +23,11 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_config, R.id.navigation_security))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun saveRecord() {
+        val sharedPrefEditor = getSharedPreferences("Name", Context.MODE_PRIVATE).edit()
+
+        sharedPrefEditor.apply()
     }
 }
--- app/src/main/java/com/viccontracovid/gamejam/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/viccontracovid/gamejam/ui/home/HomeViewModel.kt
@@ -5,5 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is configurations Fragment"
+    }
+    val text: LiveData<String> = _text
 }
